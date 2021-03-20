125014bb32a405d04a0cbfb376803742b69bc60a
Faddal Ibrahim <41176161+faddalibrahim@users.noreply.github.com>
2021-03-20 06:50:25 +0000

    add doc strings

--- Python/generate_insert_sql_commands.py
+++ Python/generate_insert_sql_commands.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python
 
+"""
+This script is intended to generate lines of sql code for insert operations
+A text file containing the lines of data to be inserted is required
+Pkus a table to be inserted into
+
+the getData() functions reads the data file and returns a list of the data
+this is then passed into the sqlise() function to generate the sql code
+
+if the data file contains this data:
+	1232021,"C090904","John Dramani Mahama","male","2000-06-13","NDC",0)
+	1232022,"C090904","Nana Addo-Danquah Akuffo Addo","male","2000-06-13","NPP",0)
+	1232023,"C090904","Nana Konadu Agyeman-Rawlings","female","2000-06-13","NDP",0)
+	1232024,"C090904","Ayariga Moses","male","2000-06-13","PNC",0)
+
+and the table name entered is Presidents
+
+ths outpute file generate will contain this:
+	insert into Presidents values(1232021,"C090904","John Dramani Mahama","male","2000-06-13","NDC",0)
+	insert into Presidents values(1232022,"C090904","Nana Addo-Danquah Akuffo Addo","male","2000-06-13","NPP",0)
+	insert into Presidents values(1232023,"C090904","Nana Konadu Agyeman-Rawlings","female","2000-06-13","NDP",0)
+	insert into Presidents values(1232024,"C090904","Ayariga Moses","male","2000-06-13","PNC",0)
+
+
+NB: this script can be easily modified to work with csv files
+
+"""
+
 file_path = input("file path: ")
 
 while file_path.strip() == 0:
@@ -26,7 +53,7 @@ def getData(file=file_path):
 
 
 def sqlise(data):
-	with open("sql_l.txt","w") as sql:
+	with open("sql.txt","w") as sql:
 		for line in data:
 			sql.write(f"insert into {table} values({line})\n")
 
